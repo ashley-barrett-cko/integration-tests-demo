e3eff8290e559ffa93b0806df45977abeaf33d14
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-01-20 13:11:40 +0000

    lastest

---
+++ src/Integration-Tests-Demo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Integration_Tests_Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
